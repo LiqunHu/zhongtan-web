e60bfade6ea48ce15d7eac85bb5977ce5ef98ab6
myway <huliquns@126.com>
2018-11-12 22:35:00 +0800

    f

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/admin',
+    assetsPublicPath: '/admin/',
     proxyTable: {
       '/api': {
         target: 'http://localhost:9090', // 接口的域名
@@ -67,7 +67,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/admin',
+    assetsPublicPath: '/admin/',
 
     /**
      * Source Maps
