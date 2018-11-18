33b6caaf0ebaa04c967f6f426d7e91c325f7ac0e
myway <huliquns@126.com>
2018-11-18 17:49:57 +0800

    f

--- web/pages/about.vue
+++ web/pages/about.vue
@@ -1,29 +1,34 @@
 <template>
-  <!-- begin #page-container -->
+    <!-- begin #page-container -->
     <div>
         <common-header></common-header>
         <div class="container">
-          <article class="page">
-            <header class="border-left entry-header">
-              <h1 class="entry-title">Chinese-Tanzanian Joint Shipping Company</h1>
-              <div class="blog-meta metabox">
-                <span class="entry-elem">
-                  <i class="fa fa-calendar"></i>
-                2018-11-16							</span>
-                <span class="separator">|</span>
-              </div>
-            </header><!-- .entry-header -->
+            <article class="page">
+                <header class="border-left entry-header">
+                    <h1 class="entry-title">中国-坦桑尼亚联合海运公司<br />
+                        Chinese-Tanzanian Joint Shipping Company</h1>
+                    <div class="blog-meta metabox">
+                        <span class="entry-elem">
+                            <i class="fa fa-calendar"></i>
+                            2018-11-16 </span>
+                        <span class="separator">|</span>
+                    </div>
+                </header><!-- .entry-header -->
 
-            <div class="entry-content">
-              <blockquote>Chinese-Tanzanian Joint Shipping Company (SINOTASHIP) was established in Tanzania in 1967 under the personal care of the late Chinese Premier Zhou Enlai and Tanzanian founding President Mwalimu K.  Nyerere. SINOTASHIP is the earliest enterprise invested by Chinese government in Tanzania, and is the beginning for the investment cooperation between China and Tanzania. Based on the principle of “friendly cooperation, equality and mutual benefit”, each government holds 50% of the Company’s share.</blockquote>
-              <blockquote>Since its establishment more than 50 years ago, SINOTASHIP has not only achieved remarkable economic benefits, but also played an important role in the political, diplomatic and economic exchanges between China and Tanzania, and has made positive contributions to enhancing friendship between the two nations. It has been praised as "a model of successful cooperation between the two governments" and "a sign of friendship between the two nations."</blockquote>
-              <blockquote>As the exclusive agent of China COSCO SHIPPING Group(who ranks No.1 in the world in terms of total fleet capacity) in Tanzania, relying on the brand advantage of COSCO SHIPPING and the Group’s vast fleets, as well as its global network, SINOTASHIP provides full supply chain services including container, bulk and specialized cargo sea transportation, integrated logistics such as hinterland transportation, warehousing, and customs clearance for all import and export customers. </blockquote>
-              <blockquote><b>SINOTASHIP, a grand bridge between East Africa and the rest of the world.</b></blockquote>
-              <blockquote><b>SINOTASHIP, your most reliable logistics service provider in East Africa.</b></blockquote>
-             </div><!-- .entry-content -->
+                <div class="entry-content">
+                    <blockquote>中国-坦桑尼亚联合海运公司（简称中坦公司），是在中国已故前总理周恩来和坦桑尼亚首任总统尼雷尔的亲自关心下，于1967年在坦桑尼亚成立。中坦公司是中国政府在坦最早投资企业，是中坦投资合作的起点。中、坦两国政府本着“友好合作、平等互利”的原则，各占公司50%股份。<br />
+                        Chinese-Tanzanian Joint Shipping Company (SINOTASHIP) was established in Tanzania in 1967 under the personal care of the late Chinese Premier Zhou Enlai and Tanzanian founding President Mwalimu K. Nyerere. SINOTASHIP is the earliest enterprise invested by Chinese government in Tanzania, and is the beginning for the investment cooperation between China and Tanzania. Based on the principle of “friendly cooperation, equality and mutual benefit”, each government holds 50% of the Company’s share.</blockquote>
+                    <blockquote>中坦公司成立50多年来，不仅取得了显著的经济效益，而且在中坦两国的政治、外交和经济交往中，发挥重要作用，为增进中坦两国人民友谊做出了积极贡献。曾被赞誉为“两国政府成功合作的典范”、“两国人民友谊的标志”。<br />
+                        Since its establishment more than 50 years ago, SINOTASHIP has not only achieved remarkable economic benefits, but also played an important role in the political, diplomatic and economic exchanges between China and Tanzania, and has made positive contributions to enhancing friendship between the two nations. It has been praised as "a model of successful cooperation between the two governments" and "a sign of friendship between the two nations."</blockquote>
+                    <blockquote>作为船队规模及综合运力排名世界第一的中国远洋海运集团在坦桑尼亚唯一指定代理，中坦公司依托中国远洋海运集团的品牌优势、集团旗下众多专业化运输船队及其遍布全球的代理机构和网络优势，中坦公司为进出口客户提供集装箱、散杂货、重大件货物海上运输，以及陆路运输和堆场、仓储、报关等门到门全程供应链服务。<br />
+                        As the exclusive agent of China COSCO SHIPPING Group(who ranks No.1 in the world in terms of total fleet capacity) in Tanzania, relying on the brand advantage of COSCO SHIPPING and the Group’s vast fleets, as well as its global network, SINOTASHIP provides full supply chain services including container, bulk and specialized cargo sea transportation, integrated logistics such as hinterland transportation, warehousing, and customs clearance for all import and export customers. </blockquote>
+                    <blockquote><b>中坦公司，将东非和世界更紧密相连。<br />
+                            SINOTASHIP, a grand bridge between East Africa and the rest of the world.</b></blockquote>
+                    <blockquote><b>中坦公司，您在东非最可信赖的物流服务商。<br />SINOTASHIP, your most reliable logistics service provider in East Africa.</b></blockquote>
+                </div><!-- .entry-content -->
 
-            <footer class="entry-footer"></footer><!-- .entry-footer -->
-          </article>
+                <footer class="entry-footer"></footer><!-- .entry-footer -->
+            </article>
         </div>
         <page-footer></page-footer>
     </div>
@@ -39,83 +44,86 @@ export default {
     CommonHeader,
     PageFooter
   },
-  head () {
+  head() {
     return {
-      title: 'About SINOTASHIP',
+      title: 'About SINOTASHIP'
     }
   },
-  mounted() {
-  },
+  mounted() {}
 }
 </script>
 <style scoped>
 .page {
-  font-family: "Open Sans", sans-serif;
+  font-family: 'Open Sans', sans-serif;
   line-height: 25px;
   -ms-word-wrap: break-word;
   word-wrap: break-word;
   color: #775f5e;
 }
 .hentry {
-    margin: 0 0 1.5em;
+  margin: 0 0 1.5em;
 }
 .border-left {
-    padding-left: 24px;
-    -webkit-box-shadow: inset 4px 0 0 0 rgba(50, 50, 50, 0.1);
-    box-shadow: inset 4px 0 0 0 rgba(50, 50, 50, 0.1);
+  padding-left: 24px;
+  -webkit-box-shadow: inset 4px 0 0 0 rgba(50, 50, 50, 0.1);
+  box-shadow: inset 4px 0 0 0 rgba(50, 50, 50, 0.1);
 }
-.entry-header h1, .entry-header h2 {
-    margin-top: 0;
-    margin-bottom: 15px;
-}
-
-.page-content, .entry-content, .entry-summary {
-    margin: 1.5em 0 0;
+.entry-header h1,
+.entry-header h2 {
+  margin-top: 0;
+  margin-bottom: 15px;
 }
 
-blockquote, q {
-    quotes: "" "";
+.page-content,
+.entry-content,
+.entry-summary {
+  margin: 1.5em 0 0;
+}
+
+blockquote,
+q {
+  quotes: '' '';
 }
 blockquote {
-    margin: 0 1.5em;
+  margin: 0 1.5em;
 }
 blockquote {
-    padding: 10px 20px;
-    margin: 0 0 20px;
-    font-size: 17.5px;
-    border-left: 5px solid #eee;
+  padding: 10px 20px;
+  margin: 0 0 20px;
+  font-size: 17.5px;
+  border-left: 5px solid #eee;
 }
 p {
   font-size: 17px;
 }
 
 .blog-meta {
-    background: none !important;
+  background: none !important;
 }
 
 .metabox {
-    color: #000;
-    clear: both;
-    background-color: rgb(221, 223, 225);
-    padding: 10px 0px 10px 0px;
-    font-size: 15px;
-    font-family: 'Poppins';
+  color: #000;
+  clear: both;
+  background-color: rgb(221, 223, 225);
+  padding: 10px 0px 10px 0px;
+  font-size: 15px;
+  font-family: 'Poppins';
 }
 
 .metabox i {
-    color: #14cab4;
-    padding: 4px;
+  color: #14cab4;
+  padding: 4px;
 }
 
 .metabox .separator {
-    color: #14cab4;
+  color: #14cab4;
 }
 
 .metabox .entry-elem {
-    padding: 0 0 0 20px;
-    margin: 0 13px 0 0;
+  padding: 0 0 0 20px;
+  margin: 0 13px 0 0;
 }
 .blog-meta .entry-elem {
-    padding-left: 0px !important;
+  padding-left: 0px !important;
 }
 </style>
--- web/pages/scope.vue
+++ web/pages/scope.vue
@@ -19,46 +19,59 @@
 
         <div class="entry-content">
           <blockquote class="clearfix">
-            <img class="attachment-img" src="/scope1.jpg">
             中坦公司拥有自营散货船舶和专业的经营管理队伍，长期从事散杂货运输，航线覆盖远东和东南亚至非洲、红海、波斯湾以及其它区域。公司将充分发挥半个世纪的航运专业经验，不断加强与货主和租家的合作，提供优质的海上运输服务和全程物流解决方案。<br />
-            SINOTASHIP owns ocean going bulk carriers with professional operating & management team, engaged in bulk & dry cargo transport, routes covering the far east and south east Asia to Africa, Red Sea, Persian Gulf, and other areas of the globe. The company will give full play to half a century of professional experience, continuously strengthen cooperation with clients and charters, provide sea transport services and door-to-door logistics solutions with the highest quality.
+            <img class="scope-img pull-left" src="/scope1.jpg">SINOTASHIP owns ocean going bulk carriers with professional operating & management team, engaged in bulk & dry cargo transport, routes covering the far east and south east Asia to Africa, Red Sea, Persian Gulf, and other areas of the globe. The company will give full play to half a century of professional experience, continuously strengthen cooperation with clients and charters, provide sea transport services and door-to-door logistics solutions with the highest quality.
           </blockquote>
-          <blockquote>As the exclusive agent of China COSCO SHIPPING Corporation Limited (who ranks no.1 in the world in terms of total fleet size and DWT capacity) and its subordinate enterprises in Tanzania, relying on China COSCO SHIPPING’s brand advantage, large-scale, quick-response, global logistics network (which consists of more than 1000 branches all over the world), with foothold in Tanzania and East Africa, SINOTASHIP provides below services to global clients:<br />
+          <blockquote>作为船队规模及综合运力排名世界第一的中国远洋海运集团及旗下企业在坦桑尼亚的唯一指定代理，依托中国远洋海运集团的品牌优势及其遍布全球的1000 多个分支及代理机构共同组成的规模庞大、服务快捷的全球物流网络优势，中坦公司立足坦桑和东非，为全球客户提供如下服务：<br />
+            As the exclusive agent of China COSCO SHIPPING Corporation Limited (who ranks no.1 in the world in terms of total fleet size and DWT capacity) and its subordinate enterprises in Tanzania, relying on China COSCO SHIPPING’s brand advantage, large-scale, quick-response, global logistics network (which consists of more than 1000 branches all over the world), with foothold in Tanzania and East Africa, SINOTASHIP provides below services to global clients:<br />
             <ol>
-              <li>Containerized Cargo Transportation Service.</li>
-              <li>Transportation Services of Bulk&General Cargo、Car&Vehicle、Timber&Log、Oversized&Overweight Cargo.</li>
-              <li>Shipping Agency Service.</li>
-              <li>Freight Forwarding & Customs Clearance.</li>
-              <li>Depot & Warehousing Services.</li>
-              <li>One-Stop Logistics Solutions.</li>
+              <li>集装箱运输服务 / Containerized Cargo Transportation Service.</li>
+              <li>散杂货、汽车、木材、以及重大件运输服务 / Transportation Services of Bulk&General Cargo、Car&Vehicle、Timber&Log、Oversized&Overweight Cargo.</li>
+              <li>船舶代理服务 / Shipping Agency Service.</li>
+              <li>货运代理及清关服务 / Freight Forwarding & Customs Clearance.</li>
+              <li>堆场及仓储服务 / Depot & Warehousing Services.</li>
+              <li>全程物流服务 / One-Stop Logistics Solutions.</li>
             </ol>
+            <img src="/scope2.jpg"><img src="/scope3.jpg">
           </blockquote>
           <ul>
             <li>
-              <h4>Containerized Cargo Transportation Service.</h4><br />
-              <blockquote>We SINOTASHIP, as agent of COSCO SHIPPING LINES CO.,LTD., provide containerized cargo transportation services to/from East Africa countries(including inland countries such as Ethiopia, Sudan, Uganda, Rwanda, Congo, Burundi, Zambia, Malawi and etc.).We have variable containers such as GP, FL, OT, Reefer for your cargo. By direct call or transshipment, we connect East Africa with Far East, South & West Asia, Middle East, Mediterranean Sea, Europe, Oceania, North & West Africa and etc. to realize your dreams of transporting cargoes between East Africa and the rest of the world.</blockquote>
+              <h4>集装箱货物运输服务 / Containerized Cargo Transportation Service.</h4><br />
+              <blockquote>我们提供进出东非区域的普柜、框架、开顶、冷藏等各种箱型的集装箱货物的海上和陆上运输。通过直航或中转服务，我们将东非和远东、南亚、西亚、中东、地中海、欧洲、大洋洲、北非、西非等紧密相连，实现将您的集装箱货物从全球运输到东非和从东非运送到全球的梦想。<br />
+                <img class="scope-img pull-left" src="/scope4.jpg"><img class="scope-img pull-right" src="/scope5.jpg">We SINOTASHIP, as agent of COSCO SHIPPING LINES CO.,LTD., provide containerized cargo transportation services to/from East Africa countries(including inland countries such as Ethiopia, Sudan, Uganda, Rwanda, Congo, Burundi, Zambia, Malawi and etc.).We have variable containers such as GP, FL, OT, Reefer for your cargo. By direct call or transshipment, we connect East Africa with Far East, South & West Asia, Middle East, Mediterranean Sea, Europe, Oceania, North & West Africa and etc. to realize your dreams of transporting cargoes between East Africa and the rest of the world.</blockquote>
             </li>
             <li>
-              <h4>Transportation Services of Bulk&General Cargo、Car&Vehicle、Timber&Log、Oversized&Overweight Cargo.</h4><br />
-              <blockquote>SINOTASHIP not only owns bulk vessels and professional management team, as agent of COSCO SHIPPING BULK CO.,LTD.and COSCO SHIPPING SPECIALIZED CARRIERS CO.,LTD.,we can also provide transportation services of general & bulk cargo、car & vehicle、timber&log and awkward cargo to/from East Africa to our global clients. Our professional services as well as reasonable prices are your best choices.</blockquote>
+              <h4>散杂货、汽车、木材及重大件货物运输服务 / Transportation Services of Bulk&General Cargo、Car&Vehicle、Timber&Log、Oversized&Overweight Cargo.</h4><br />
+              <blockquote>中坦公司不仅拥有自营散货船舶和专业的经营队伍，作为中远海运散货运输有限公司和中远海运特种运输股份有限公司在东非的代理，我们可以为全球客户提供东非进出口散杂货、汽车、木材及重大件货物运输服务。专业的服务，合理的价格，是您不二的选择。<br />
+                <img class="scope-img pull-left" src="/scope6.jpg"><img class="scope-img pull-right" src="/scope7.jpg">SINOTASHIP not only owns bulk vessels and professional management team, as agent of COSCO SHIPPING BULK CO.,LTD.and COSCO SHIPPING SPECIALIZED CARRIERS CO.,LTD.,we can also provide transportation services of general & bulk cargo、car & vehicle、timber&log and awkward cargo to/from East Africa to our global clients. Our professional services as well as reasonable prices are your best choices.</blockquote>
             </li>
             <li>
-              <h4>Shipping Agency Service.</h4><br />
-              <blockquote>We provide professional and diversified agency services for all vessels calling Tanzania and East Africa. Our long-term close relationship with local government administrations such as Port Authorities, Customs, and Maritime Safety Administrations ensure that we are able to provide more satisfactory services to you. </blockquote>
+              <h4>船舶代理服务 / Shipping Agency Service.</h4><br />
+              <blockquote>我们可以为您到访东非港口的船舶办理进出港手续，安排引水靠泊、装卸，为船舶安排加油加水、代购食品和用品等。我们同当地海关海事港务等政府部门长期良好的关系,确保我们可以提供更令您满意的服务。<br />
+                We provide professional and diversified agency services for all vessels calling Tanzania and East Africa. Our long-term close relationship with local government administrations such as Port Authorities, Customs, and Maritime Safety Administrations ensure that we are able to provide more satisfactory services to you. </blockquote>
             </li>
             <li>
-              <h4>Freight Forwarding & Customs Clearance.</h4><br />
-              <blockquote>We provide services such as booking, customs clearance & quarantine, land transportation, issuing B/L, exchanging D/O and etc. We are engaged in safer and more prompt cargo transportation.</blockquote>
+              <h4> 货运代理及清关服务 / Freight Forwarding & Customs Clearance.</h4><br />
+              <blockquote><img class="scope-img pull-left" style="width: 230px" src="/scope8.jpg">
+                我们可以接受您的委托，为您的进出口货物安排报关报检、陆路运输、海运订舱、出口签单和进口换单等，让您的货物安全、快捷地到达您或您的客户手中。<br />
+                We provide services such as booking, customs clearance & quarantine, land transportation, issuing B/L, exchanging D/O and etc. We are engaged in safer and more prompt cargo transportation.
+              </blockquote>
             </li>
             <li>
-              <h4>Depot & Warehousing Services.</h4><br />
-              <blockquote>We own depot and warehouse and provide depot and warehousing services including cargo receiving & releasing, storage, packaging, boxing & unboxing, LCL and etc.</blockquote>
+              <h4>堆场&仓储服务 / Depot & Warehousing Services.</h4><br />
+              <blockquote><img class="scope-img pull-right" style="width: 230px" src="/scope9.jpg">我们拥有自有堆场和仓库，可以为您提供货物的收发、交接、保管、存储、搬运、包装及集装箱货物的装箱、施封、拆箱及拼箱服务。<br />We own depot and warehouse and provide depot and warehousing services including cargo receiving & releasing, storage, packaging, boxing & unboxing, LCL and etc.</blockquote>
             </li>
             <li>
-              <h4>One-Stop Logistics Solutions.</h4><br />
-              <blockquote>With the background of China COSCO SHIPPING’s professional vibrant talent team, powerful capacity on variable transportations including container, bulk & general cargo, energy, specialized cargo and comprehensive logistics, global business network, as well as SINOTASHIP’s 50-years deep plough in East Africa, we can provide one-stop & door-to-door logistics solutions to your cargoes to and from East Africa region. A commitment to us, a worry-free journey to you!</blockquote>
+              <h4>全程物流服务 / One-Stop Logistics Solutions.</h4><br />
+              <blockquote>凭借中远海运集团在集装箱、散杂货、能源、特种运输等各个专业运输领域和综合物流方面强大的实力、专业的人才队伍、遍布全球的物流服务网络和中坦公司在东非市场50年的深耕细作，我们可以为您进出东非区域的货物提供一站式、门到门的全程物流服务，让您一次托付，全程无忧。<br />
+                With the background of China COSCO SHIPPING’s professional vibrant talent team, powerful capacity on variable transportations including container, bulk & general cargo, energy, specialized cargo and comprehensive logistics, global business network, as well as SINOTASHIP’s 50-years deep plough in East Africa, we can provide one-stop & door-to-door logistics solutions to your cargoes to and from East Africa region. A commitment to us, a worry-free journey to you!</blockquote>
             </li>
           </ul>
+          <blockquote><b>中坦公司，将东非和世界更紧密相连。<br />
+              SINOTASHIP, a grand bridge between East Africa and the rest of the world.<br />
+              中坦公司，您在东非最可信赖的物流服务商。<br />
+              SINOTASHIP, your most reliable logistics service provider in East Africa. <br />
+            </b></blockquote>
         </div><!-- .entry-content -->
 
         <footer class="entry-footer"></footer><!-- .entry-footer -->
@@ -159,5 +172,9 @@ p {
 }
 .blog-meta .entry-elem {
   padding-left: 0px !important;
+}
+
+.scope-img {
+  width: 280px;
 }
 </style>
