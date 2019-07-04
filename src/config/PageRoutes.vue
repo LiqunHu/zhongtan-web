04eb517fc34aba56d3a3b607cf92ca68799f548f
LiqunHu <huliquns@126.com>
2019-07-04 14:08:51 +0800

    fix

--- src/config/PageRoutes.vue
+++ src/config/PageRoutes.vue
@@ -1,10 +1,10 @@
 <script>
 const routes = [
   { path: '*', component: resolve => require(['@/components/error404'], resolve) },
-  { path: '/admin', component: resolve => require(['@/views/Login'], resolve) },
+  { path: '/admin', component: resolve => require(['@/views/login'], resolve) },
   { path: '/error', component: resolve => require(['@/components/error'], resolve) },
   { path: '/error404', component: resolve => require(['@/components/error404'], resolve) },
-  { path: '/admin/dashboard/home', component: resolve => require(['@/views/Home'], resolve) },
+  { path: '/admin/dashboard/home', component: resolve => require(['@/views/home'], resolve) },
   { path: '/admin/common/system/SystemApiControl', component: resolve => require(['@/views/common/system/SystemApiControl'], resolve) },
   { path: '/admin/common/system/GroupControl', component: resolve => require(['@/views/common/system/GroupControl'], resolve) },
   { path: '/admin/common/system/OperatorControl', component: resolve => require(['@/views/common/system/OperatorControl'], resolve) },
